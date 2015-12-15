e56f30f3a65fdf36301ec5bb4a38d668a5b567e2
Aditya Anchuri <aanchuri@pivotal.io>
2015-12-14 17:44:54 -0800

    Replaced bosh and bosh-lite scripts with ones that read environment info and creds from lock files, updated CI pipeline
    [#109984560]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

---
+++ scripts/utils.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+check_if_deployment_exists () {
+    DEPLOYMENT_NAME=${1:?"Pass deployment name as first argument"}
+    deployments_output=$( bosh deployments )
+    echo ${deployments_output} | grep ${DEPLOYMENT_NAME}
+}
+
+jq_val() {
+    JSON_KEY="${1:?Expected key name as first arg}"
+    JSON_FILE="${2:?Expected JSON file as second arg}"
+
+    RESULT="$(jq -r ".${JSON_KEY}" "${JSON_FILE}")"
+    if [[ "$RESULT" == "null" ]]; then
+        echo "Could not find key '${JSON_KEY}' in file '${JSON_FILE}'"
+        exit 1
+    fi
+    echo "${RESULT}"
+}
